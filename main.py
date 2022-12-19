f6f0c19436f180a404a26d34758b04b4ef50c7a8
DanielSamuels <35511897+danielfishbone@users.noreply.github.com>
2022-12-19 00:16:55 +0000

    removed irrelevant stuff

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
 #Password Generator Project
 from random import choice
 from random import randint
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+letters = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+    'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-
-
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 list_of_char = []
 for sample in range(nr_letters):
-  list_of_char.append("L")
+    list_of_char.append("L")
 for sample in range(nr_symbols):
-  list_of_char.append("S")
+    list_of_char.append("S")
 for sample in range(nr_numbers):
-  list_of_char.append("N")  
+    list_of_char.append("N")
 password = ""
-print (f"Your password length is: {len(list_of_char)}")
+print(f"Your password length is: {len(list_of_char)}")
 for i in range(len(list_of_char)):
-  selected = randint(0,len(list_of_char)-1)
-  val = list_of_char.pop(selected)
-  if val == "L":
-    password += str(choice(letters))
-  elif val == "S":
-    password += str(choice(symbols))
-  elif val == "N":
-    password += str(choice(numbers))
-    
-print (f"Your password is: {password}")
+    selected = randint(0, len(list_of_char) - 1)
+    val = list_of_char.pop(selected)
+    if val == "L":
+        password += str(choice(letters))
+    elif val == "S":
+        password += str(choice(symbols))
+    elif val == "N":
+        password += str(choice(numbers))
+
+print(f"Your password is: {password}")
